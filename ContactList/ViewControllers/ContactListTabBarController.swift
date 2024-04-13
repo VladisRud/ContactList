8b0958a8499c261ea90a96d220eedf587079524b
Vladislav Rudenko <vladisrud@gmail.com>
2024-04-13 23:11:32 +0400

    Initial Commit

---
+++ ContactList/ViewControllers/ContactListTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  ContactListTabBarController.swift
+//  ContactList
+//
+//  Created by Влад Руденко on 13.04.2024.
+//
+
+import UIKit
+
+class ContactListTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ ContactList/ViewControllers/StartContactListController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Влад Руденко on 13.04.2024.
+//
+
+import UIKit
+
+class StartContactListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemCyan
+    }
+
+
+}
+
